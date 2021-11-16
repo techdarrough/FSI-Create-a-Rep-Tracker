ddb0525609cfdae655dd37ee6516d184c01ccc9b
Preston Darrough <techdarrough@gmail.com>
2021-11-15 22:19:49 -0800

    rep tracker commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your Code Here
+let reps;
+let serializedReps = localStorage.getItem('reps')
+if(serializedReps === null){
+    reps = []
+} else {
+    reps = JSON.parse(serializedReps)
+}
+let newNumberOfReps = window.prompt('Enter your number of reps: ')
+reps.push(newNumberOfReps)
+renderChart(reps)
+serializedReps = JSON.stringify(reps)
+localStorage.setItem('reps', serializedReps)
